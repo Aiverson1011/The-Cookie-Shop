0bec6c8505f2dc8c52cf59a7b8384faf6a95c688
Amanda Iverson <ajohnson764@gmail.com>
2022-06-27 20:43:29 -0700

    day 7 lecture

--- app.js
+++ app.js
@@ -1,56 +1,53 @@
 'use strict'
-// use strict tells us to follow some baseline 'strict rules' 
 
-// variables = storing data as values. 
 
-// Comment out in mass 
-// highlight all the lines
-// CTRL + K + C
-
-// declaring the variable firstname to be equal to the value of amanda
-// let firstName = 'Amanda'; // string
-// let lastName = "Iverson"; // single or double quotes are acceptable
-// let numberOfCats = 2; // number
-// let lovesCoffee = true; // boolean
-
-// dynamic 
-let firstName = prompt('What is your Name?');
-let time = prompt('What hour is it (0-24?)');
-let message = '';
-// of the time of day is before noon then we want to say "Good Morning"
-
-// other comparisons:
-// <= less than or equal too
-// < less than
-// >= greater than or equal too
-// > greater than
-// === strict equal
-// == loose equal (comparing numeric to strings)
-if(time <= 11){
-    message = 'Good Morning!!';
-    //console.log('Good Morning!!');
-} else if(time <= 18){
-    message = 'Good Afternoon!!';
-   // console.log('Good Afternoon!!');
-} else if(time <= 24){
-    message = 'Good Night!!';
-    //console.log('Good Night!');
-} else{
-    message = 'Good Day!!';
-   // console.log('Have a Great Day!');
+function generateGreeting(){
+    let firstName = getFirstName(); // Mango
+    let message = timeOfDay(); // Good Afternoon
+    return document.write('Hello ' + firstName + ', ' + message + '!');
 }
 
-// console.log('done with program');
+function getFirstName(){
+    let firstName = prompt('What is your Name?'); //Mango
+    return firstName;
+}
 
-document.write('Hello '+ firstName + '! ' + message);
 
-// Amanda is not equal to amanda
-// === strict equals
-// == loose equals (truthy or a fasly) '1' == 1
-// if(firstName.toLowerCase() === 'amanda'){
-//     console.log('Your name is amanda!');
+function timeOfDay(){
+    let time = prompt('What hour is it (0-24?)');
+    let message = '';
+    
+    
+    if(time <= 11 && time > 0){
+        message = 'Good Morning!!';
+    } else if(time <= 18){
+     message = 'Good Afternoon!!';
+    } else if(time <= 24){
+     message = 'Good Night!!';
+    } else{
+      message = 'Good Day!!';
+    }
+
+    return message;
+}
+
+
+function getCookiePicture1(){
+    let answer = prompt ('pick 1 or 2');
+    let image = '';
+    if(answer == 1){
+        image = '<img class="subImage" alt="cookie image 1" src="https://via.placeholder.com/300x150.png?text=Cookie+pic+1">'
+    } else {
+        image = '<img class="subImage" alt="cookie image 1" src="Cookie.jpg">'
+
+    }
+    return document.write(image);
+}
+
+
+
+// getGreeting(firstName);
+// function getGreeting(yourName){
+//     console.log('Hello, ' + yourName );
+//     console.log('this was run from the function');
 // }
-
-// output to the console some value
-//console.log('your age is: ' + age);
-//  console.log('You have ' + numberOfCats + ' cats');
