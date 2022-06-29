a2497ee716891f0710aac546a6ac3117ad04c581
Amanda Iverson <ajohnson764@gmail.com>
2022-06-28 21:34:48 -0700

    new day of demo!

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ function getFirstName(){
     return firstName;
 }
 
-
 function timeOfDay(){
     let time = prompt('What hour is it (0-24?)');
     let message = '';
@@ -31,7 +30,6 @@ function timeOfDay(){
     return message;
 }
 
-
 function getCookiePicture1(){
     let answer = prompt ('pick 1 or 2');
     let image = '';
@@ -44,10 +42,86 @@ function getCookiePicture1(){
     return document.write(image);
 }
 
+// while loop
+// while(some condition is true){
+// do something over and over and over again
+//}
+
+// guessNumberOfCats();
+function guessNumberOfCats(){
+// string !== string (Strict, not equals only used for similar data types)
+// string === string (strict equals)
+// string != int (loose, not equals use when crossing data types)
+// string == int (loose, equals numeric to a string comparison)
+
+//stringify (converting something to a string)
+
+let answer = prompt('how many cats does Amanda have?'); // 3
+
+    while(answer != 2){
+        if(answer < 2){
+            alert('too low');
+        } else {
+            alert('too high!');
+        }
+        answer = prompt('how many cats does Amanda have?'); // 2
+    }
+
+    alert('Congrats! you are correct!');
+
+    let x = 0;
+
+    while(x < 5){
+        console.log(x);
+        // need to do something with X so that it eventually becomes >= 5.
+        //x = x + 1;
+        x++;
+    }
+    
+    console.log("i am done");
+}
+
+// doWhileExample();
+function doWhileExample(){
+
+    // do...while guarantees us at least ONE iteration
+    let answer = '';
+    do{
+        answer = prompt('Guess a number between 1 - 5'); // 3
+        if(answer < 3 ){
+            alert('too low');
+        } else if( answer > 3){
+            alert('too high');
+        } else{
+            alert('CORRECT! The answer is 3!');
+        }
+    }while(answer != 3)
+
+    console.log("Completed");
+
+}
+
+// forLoopExample();
+function forLoopExample(){
+    // 1. variable declaration & assignment
+    // 2. condition that needs to be met
+    // 3. what do we do with the variable once an iteration has completed
+
+    let answer = prompt('How many stars would rate our cookie shop? (1 -5)'); // 3
+ 
+    let output = '';
+    for(let x = 0; x < answer; x++){
+        output = output + "<img class='star' src='star.jpg' />"
+
+    }
+    //output = <img class='star' src='star.jpg' />
+    // output =  <img class='star' src='star.jpg' /> <img class='star' src='star.jpg' />
+    // output =  <img class='star' src='star.jpg' /> <img class='star' src='star.jpg' /> <img class='star' src='star.jpg' />
 
 
-// getGreeting(firstName);
-// function getGreeting(yourName){
-//     console.log('Hello, ' + yourName );
-//     console.log('this was run from the function');
-// }
+    console.log("i'm am done");
+
+    return document.write(output);
+
+}
+
